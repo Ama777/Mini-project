976e27268777952fe16fb744cb23b3e6ebd32d1b
Indukilana Botheju <bothejuindukilana@gmail.com>
2020-11-18 22:12:38 +0530

    updated
    Added Reports.html and Accounts_info.html pages to the project.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ let btn_delete = document.getElementById("deleted");
 function get_update(){
     let title = document.getElementById('title').value;
     let desc = document.getElementById('description').value;
-    arr.push([title, desc]);
+    let asd = document.getElementById('assdate').value;
+    let dedate = document.getElementById('dedate').value;
+    let sts = document.getElementById('status').value;
+    arr.push([title, desc, asd, dedate, sts]);
     update();
    
 
@@ -20,6 +23,10 @@ function update(){
         <th scope="row">${index+1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
+        <td>${element[2]}</td>
+        <td>${element[3]}</td>
+        <td>${element[4]}</td>
+        <td><button type="submit" class="btn btn-primary btn-sm " onclick="updated(${index})" >Update</button></td>
         <td><button type="submit" class="btn btn-primary btn-sm " onclick="deleted(${index})" >Delete</button></td>
         </tr>`
     });
